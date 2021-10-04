bcb195d20d20baa50a81998c82293f877691e634
IINEP7UNEII <dantolsky@gmail.com>
2021-10-04 12:50:10 -0700

    finish isPrime and isPerfectSquare

--- Assignments/Assignment 2/factors.cpp
+++ Assignments/Assignment 2/factors.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <iomanip>
-#include <cmath>
 using namespace std;
 
 //function prototypes
 int validValue (int);
-bool isPerfectSquare (int);
-bool isPrime (int);
+string isPerfectSquare (int);
+string isPrime (int);
 
 int main()
 {
@@ -39,7 +38,7 @@ int main()
                 ++factorCount;
             }
         }
-        cout << '(' << factorCount << ')' << isPerfectSquare(currentNum) << isPrime(currentNum) << '\n';
+        cout << '(' << factorCount << ')' << isPerfectSquare(currentNum) << isPrime(factorCount) << '\n';
         factorCount = 0;
     }
 
@@ -55,26 +54,35 @@ int validValue(int input)
         cout << "The value must be greater than 0 and less than 1000: ";
         cin >> input;
     }
+
     return input;
 }
 
-bool isPerfectSquare (int num)
+//function that determines whether a number is a perfect square
+//the sqaure root of a perfect square is an integer, so if we sqrt the value and assign it to an integer and then multiply it to itself, then we should get the original number back
+string isPerfectSquare (int num)
 {
-    int square = 0;
-    
-    square = sqrt(num);
-
-    if ((square * square) == num)
+    for (int square = 1; square * square <= num; square++) 
     {
-        return true;
+        if ((num % square == 0) && (num / square == square)) 
+        {
+            return "  **perfect square**";
+        }
+    }
+    return {};
+}
+
+//function determines whether a number is prime by determening if the number of factors is has is equal to 2
+string isPrime (int factors)
+{
+    if (factors == 2)
+    {
+        return "  **prime**";
     }
     else
     {
-        return false;
+        return {};
     }
 }
 
-bool isPrime (int num) //continue building funcitons
-{
-
-}
+//continue interesting numbers function
