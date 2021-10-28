e0ef34573529d3d601de6a59969b38babc682206
IINEP7UNEII <dantolsky@gmail.com>
2021-10-27 18:14:11 -0700

    finished assignment 3

--- Assignments/Assignment 3/numbers.cpp
+++ Assignments/Assignment 3/numbers.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
 #include <string>
-#include <cmath> //remove if not using
 using namespace std;
 
 //function prototypes
 string intToString(int);
 void printIntStrings(int, int, int);
+string addWordsToString(int);
 string ones(int);
 string teens(int);
 string tens(int);
 string hundreds(int);
 string thousands(int);
 
-int main() //only used for testing purposes, delete afterwards
+//global variable used to set max and min values of numbers
+const unsigned int MIN_VALUE = 0;
+const unsigned int MAX_VALUE = 99'999;
+
+/*int main() //only used for testing purposes, comment out afterwards
 {
     int start = 0;
     int end = 0;
@@ -30,63 +34,72 @@ int main() //only used for testing purposes, delete afterwards
     cout << "\n\n";
 
     system("pause");
-}
+}*/
 
 //intToString definition
 string intToString(int number)
 {
-    string err = "error";
-    string s = "";
+    string err = "error"; //error message
+    string numberWord = "";
 
-    if (number > 0 && number <= 99999)
+    if (number > MIN_VALUE && number <= MAX_VALUE) //test if number is valid (in max and min bounds)
     {
-        s += tens(number);
-        s += teens(number);
-        s += ones(number);
+        numberWord += addWordsToString(number);
     }
-    else if (number == 0)
+    else if (number == 0) //test if number == 0, then exception
     {
-        s = "zero";
+        numberWord = "zero";
     }
-    else
+    else //error message if number is out of bounds
     {
         cerr << err;
-
     }
-    return s;
+    return numberWord;
 }
 
 //printIntStrings definition
-void printIntStrings(int start, int end, int interval) //change to include commas if number is greater than one-thousand. Ex: "1,000" or " 36,000"
+void printIntStrings(int start, int end, int interval)
 {
     if (end > start) //ascending order
     {
         for (int count = start; count <= end; count += interval)
         {
-            cout << count << " - " << intToString(count) << '\n';
+            cout << intToString(count) << '\n';
         }
     }
     else if (start > end) //descending order
     {
         for (int count = start; count >= end; count -= interval)
         {
-            cout << count << " - " << intToString(count) << '\n';
+            cout << intToString(count) << '\n';
         }
     }
     else //if start = end
     {
-        cout << start;
+        cout << intToString(start) << '\n';
     }
+}
+
+string addWordsToString(int num) //function that adds all number cases to a single string
+{
+    string numberWord = "";
+
+    numberWord += thousands(num);
+    numberWord += hundreds(num);
+    numberWord += tens(num);
+    numberWord += teens(num);
+
+    return numberWord;
 }
 
 string ones(int num) //returns strings for ones
 {
     string word = "";
-    int range = num / 10;
+    int range = num / 10; //set range so we are only looking at the ones digits of the number
 
-    if (range % 10 != 1)
+    if (range % 10 != 1) //checks if the ones number is related to teens (if 21 - twenty one, or 11 - eleven)
     {
-        switch (num % 10)
+        switch (num % 10) //switch ignore case 0 as that is an exception handled in the intToString function
         {
             case 1:
             {
@@ -145,9 +158,9 @@ string ones(int num) //returns strings for ones
 string teens(int num) //returns strings for teens
 {
     string word = "";
-    int range = num / 10;
+    int range = num / 10; //sets range for teens
 
-    if (range % 10 == 1)
+    if (range % 10 == 1) //if range % 10 == 1 then we do the switch function, because if it is greater or less than that we call the tens funciton
     {
         switch (num % 20)
         {
@@ -213,9 +226,9 @@ string teens(int num) //returns strings for teens
 string tens(int num) //returns strings for tens, calls ones function
 {
     string word = "";
-    int range = num / 10;
+    int range = num / 10; //range used to count digits of tens
 
-    switch (range % 10)
+    switch (range % 10) //swtich from tens = 2 to tens = 9 (not including tens = 1 as the teens funciton handles those numbers; also not including tens = 0 as we ignore that in writing) 
     {
         case 2:
         {
@@ -263,25 +276,63 @@ string tens(int num) //returns strings for tens, calls ones function
         }
     }
     
-    if (range % 10 >= 2 && num % 10 != 0)
+    if (range % 10 >= 2 && num % 10 != 0) //adds a dash if there is a ones number afterwards that is not teens
     {
         word += "-";
     }
 
-    ones(num);
+    word += ones(num); //call ones function
     return word;
 }
 
 string hundreds(int num)
 {
     string word = "";
-    //calls some preceding functions
+    int range = num / 100; //range used to count digits of hundreds
+
+    if (range % 10 != 0) //checks if hundreds is not 0
+    {
+        word += ones(range % 10); //calls ones function to determine how many hundreds ("two" hundred or "three" hundred...)
+        word += " hundred";
+
+        if (num % 100 != 0) //adds "and" if numbers below hundreds are not all zero (one hunred "and" one, opposed to one hundred "")
+        {
+            word += " and ";
+        }
+    }
     return word;
 }
 
 string thousands(int num)
 {
     string word = "";
-    //calls some preceding functions
+    int range = num / 1000; //range used to count digits of tousands
+    int rangeForComma = num / 100; //range used in the if statement below that determines whether a commma is appropriate at the number
+
+    if (range > 0) //checks if thousands is not 0
+    {
+        if (range >= 20)
+        {
+            word += tens(range); //calls tens function to determine how many thousands ("twenty" thousand or "thirty" thousand...)
+        }
+        else if (range >= 10 && range < 20)
+        {
+            word += teens(range); //calls teens function to determine how many thousands ("twelve" thousand or "thirteen" thousand...)
+        }
+        else
+        {
+            word += ones(range % 10); //calls ones function to determine how many thousands ("two" thousand or "three" thousand...)
+        }
+        word += " thousand";
+
+        if (rangeForComma % 10 != 0) //used to determine if a comma is necessary for this number
+        {
+            word += ", ";
+        }
+        else if (num % 100 != 0) //adds "and" if numbers below hundreds are not all zero (one hunred "and" one, opposed to one hundred "")
+        {
+            word += " and ";
+        }
+    }
     return word;
 }
