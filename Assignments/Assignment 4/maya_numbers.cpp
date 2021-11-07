be7cd441b77311239940ebea389f0f19238b42d4
IINEP7UNEII <dantolsky@gmail.com>
2021-11-06 18:56:47 -0700

    started assignment 4

---
+++ Assignments/Assignment 4/maya_numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+void printMayanNumber (int);
+
+int main()
+{
+
+    
+    system("pause");
+}
+
+void printMayanNumber (int num)
+{
+
+}
