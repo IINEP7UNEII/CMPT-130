2c879bf6954bdcbcfcbb511710d34037e8a6cdb7
IINEP7UNEII <dantolsky@gmail.com>
2021-11-09 21:56:32 -0800

    prototype game (start rubric check)

--- Assignments/Assignment 4/maya_numbers.cpp
+++ Assignments/Assignment 4/maya_numbers.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <ctime>
 using namespace std;
 
-//function definitions
+//function definitions to display mayan numbers
 void printMayanNumber(int);
 int powOfTwenty(int);
 void printOnes(int);
@@ -13,18 +14,23 @@ int numberOfFives(int);
 void printZero(int);
 void printSeparator();
 
+//function definitions for mayan number game
+void mayaNumberGame();
+int readNumber();
+char readLetter();
+int randomizerForNumbers(int);
+char randomizerForABC();
+
 const int MAYAN_NUMBER_BASE = 20; //vigesimal
+const int MIN_INPUT_NUMBER = 10; //if value enetered is below this, the game will end
 
 int main() //main function that only calls printMayanNumber
 {
-    int num = 0;
+    printMayanNumber(5212); //print the maya representation of the number 5,212 as per assignment requirements
+    cout << "\n\n\n"; //for formatting to make it look uncluttered
 
-    cout << "Enter number to convert into Mayan number: ";
-    cin >> num;
-    cout << "\n\n";
-
-    printMayanNumber(num);
-    cout << '\n';
+    mayaNumberGame();
+    cout << '\n'; //for formatting to make it look uncluttered
 
     system("pause");
 }
@@ -59,7 +65,6 @@ void printMayanNumber (int num) //prints mayan numbers by calling other function
 
     delete[] mayanArray; //delete dynamic array
 }
-
 
 int powOfTwenty(int num) //to determine the number of digits in the a base 20 version of the number
 {
@@ -136,4 +141,144 @@ void printSeparator() //funciton prints seperator between base 20s
 {
     const string split = "______";
     cout << split << "\n";
+}
+
+void mayaNumberGame()
+{
+    int number = 0;
+    char letter = ' ';
+    char correctLetter = ' ';
+    int correctNumber = 0;
+
+    cout << "###MAYAN NUMBER GAME###\n=======================\n\n"
+    << "Enter a number greater than 10. The game will choose a number less than or equal to what you entered.\n"
+    << "You will then have to match the decimal number to it's Mayan equivalent."
+    << "\nEnter a positive integer value greater or equal than 10 (enter a value less than 10 to exit): ";
+    number = readNumber();
+    srand(time(NULL));
+
+    while (number >= MIN_INPUT_NUMBER) //game loop
+    {
+        correctLetter = randomizerForABC();
+        correctNumber = randomizerForNumbers(number);
+
+        cout << "Which of these Mayan numbers is equal to " << correctNumber << '?';
+
+        cout << "\n\na.\n";
+        if (correctLetter == 'a')
+        {
+            printMayanNumber(correctNumber);
+        }
+        else
+        {
+            printMayanNumber(randomizerForNumbers(number));
+        }
+
+        cout << "\nb.\n";
+        if (correctLetter == 'b')
+        {
+            printMayanNumber(correctNumber);
+        }
+        else
+        {
+            printMayanNumber(randomizerForNumbers(number));
+        }
+
+        cout << "\nc.\n";
+        if (correctLetter == 'c')
+        {
+            printMayanNumber(correctNumber);
+        }
+        else
+        {
+            printMayanNumber(randomizerForNumbers(number));
+        }
+
+        cout << "\nEnter a, b, or c: ";
+        letter = readLetter();
+
+        if (letter == correctLetter)
+        {
+            cout << "\n\nCORRECT! The Mayan representation of " << correctNumber << " is:\n";
+            printMayanNumber(correctNumber);
+        }
+        else
+        {
+            cout << "\n\nINCORRECT! The Mayan representation of " << correctNumber << " is:\n";
+            printMayanNumber(correctNumber);
+        }
+
+        cout << "\n\nEnter another number, or less than 10 to exit: ";
+        number = readNumber();
+        cout << '\n';
+    }
+}
+
+int readNumber() 
+{
+    int input = 0;
+	cin >> input;
+
+	while (cin.fail()) 
+	{
+		cerr << "ERROR: Please enter an integer value: " << endl;
+		if (cin.fail()) 
+		{
+			cin.clear();
+			cin.ignore(10000, '\n');
+		}
+		cin >> input;
+	}
+	cin.ignore(10000, '\n');
+	return input;
+}
+
+char readLetter()
+{
+    char input = ' ';
+	cout << "\n\nPlease enter either 'a', 'b', or 'c': ";
+	cin >> input;
+
+	while (input != 'a' && input != 'b' && input != 'c') 
+	{
+		cerr << "ERROR: Please enter either 'a', 'b', or 'c': " << endl;
+		if (cin.fail()) 
+		{
+			cin.clear();
+			cin.ignore(10000, '\n');
+		}
+		cin >> input;
+	}
+	cin.ignore(10000, '\n');
+	return input;
+}
+
+int randomizerForNumbers(int max)
+{
+    int min = 4;
+    int random = (rand() % max) + min; //min number is 4 due to assignment requirements
+    return random;
+}
+
+char randomizerForABC()
+{
+    char choice = ' ';
+    int max = 3; //max = 3 because 3 choices (a, b, c)
+    int min = 1; //min = 1 so we have 3 choices not 4
+
+    int random = (rand() % max) + min;
+
+    if (random == 1)
+    {
+        choice = 'a';
+    }
+    else if (random == 2)
+    {
+        choice = 'b';
+    }
+    else
+    {
+        choice = 'c';
+    }
+    return choice;
 }
