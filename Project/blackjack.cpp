4c89c4210499eb6378e5bd13a07fa18ec71fd0c9
IINEP7UNEII <dantolsky@gmail.com>
2021-11-22 19:57:50 -0800

    started project

---
+++ Project/blackjack.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+struct Card
+{
+    string suit;
+    string description;
+    int rank;
+    int value;
+}; //continue make constructor
+
+int main()
+{
+
+
+    system("pause");
+}
