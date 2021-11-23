087b588f169b08efd61eb3213bea6df93f5f6737
IINEP7UNEII <dantolsky@gmail.com>
2021-11-23 03:37:34 -0800

    created and initialized structs

--- Project/blackjack.cpp
+++ Project/blackjack.cpp
@@ -7,11 +7,32 @@ struct Card
     string description;
     int rank;
     int value;
-}; //continue make constructor
+
+    Card()
+    {
+        suit = "";
+        description = "";
+        rank = 0;
+        value = 0;
+    }
+};
+
+struct CardArray
+{
+    Card* cards;
+    int cardsSize;
+    int usedCards;
+
+    CardArray()
+    {
+        cards = nullptr;
+        cardsSize = 0;
+        usedCards = 0;
+    }
+};
 
 int main()
 {
-
 
     system("pause");
 }
